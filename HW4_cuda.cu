1978838cc34b0a266ddf3e415a3ff2d81c7c3f98
wengbrian <wengbrian1116@gmail.com>
2017-12-17 22:41:27 +0800

    testcase1 pass

--- HW4_cuda.cu
+++ HW4_cuda.cu
@@ -8,12 +8,13 @@ void input(char *inFileName);
 void output(char *outFileName);
 int *d_ptr;
 size_t pitch;
+int debug = 0;
 
 void copyTo();
 void copyBack();
 void block_FW(int B);
 int ceil(int a, int b);
-__global__ void cal(int B, int Round, int block_start_x, int block_start_y, int block_width, int block_height, int n, int *d_ptr, size_t pitch);
+__global__ void cal(int B, int Round, int block_start_x, int block_start_y, int block_width, int block_height, int n, int *d_ptr, size_t pitch, int num_block, int debug);
 
 int n, m;	// Number of vertices, edges
 static int Dist[V][V];
@@ -72,110 +73,221 @@ int ceil(int a, int b)
 }
 
 void copyTo(){
+    if(debug){
+        printf("\nhost:\n");
+        for(int i = 0; i < 5; i++){
+            int *row =  Dist[i];
+            for(int j = 0; j < 5; j++){
+                printf("%d ", row[j]);
+            }
+            printf("\n");
+        }
+    }
     cudaMallocPitch(&d_ptr, &pitch, n * sizeof(int), n);
-    cudaMemcpy2D(d_ptr, pitch, &Dist, n * sizeof(int), n, n, cudaMemcpyHostToDevice);
+    cudaMemcpy2D(d_ptr, pitch, &Dist, V * sizeof(int), n * sizeof(int), n, cudaMemcpyHostToDevice);
 }
 
 void copyBack(){
-    cudaMemcpy2D(&Dist, n * sizeof(int), d_ptr, pitch, n, n, cudaMemcpyDeviceToHost);
+    cudaMemcpy2D(&Dist, V * sizeof(int), d_ptr, pitch, n * sizeof(int), n, cudaMemcpyDeviceToHost);
+    if(debug){
+        printf("\nhost:\n");
+        for(int i = 0; i < 5; i++){
+            int *row =  Dist[i];
+            for(int j = 0; j < 5; j++){
+                printf("%d ", row[j]);
+            }
+            printf("\n");
+        }
+    }
+}
+__global__
+void test(int *d_ptr, size_t pitch, int n){
+    printf("\ndevice:\n");
+    for(int i = 0; i < n; i++){
+        int *row = (int*) ((char*)d_ptr + i * pitch);
+        for(int j = 0; j < n; j++){
+            printf("%d ", row[j]);
+        }
+        printf("\n");
+    }
 }
 
 void block_FW(int B)
 {
 	int round = ceil(n, B);
-    int num_block = ceil(B*B, 1024);
-    int size = B*B*sizeof(int);
-    num_block = ceil(B, 32);
-    dim3 grid(num_block, num_block);
-    size = 1024*sizeof(int)*3;
-    size = 48*1024;
+    int size = 3*1024*sizeof(int);
+    dim3 block(32,32);
+    int num_block = ceil(B,32);
 	for (int r = 0; r < round; ++r) {
-        printf("%d %d\n", r, round);
+        if(debug)
+            printf("%d %d\n", r, round);
+
 		/* Phase 1*/
-		cal<<<grid,1024,size>>>(B,	r,	r,	r,	1,	1, n, d_ptr, pitch);
-		//printf("%d ", cal(B,	r,	r,	r,	1,	1));
-        // sync
+        int num_block_width = ceil(B,32);
+        int num_block_height = ceil(B,32);
+        dim3 grid(num_block_width, num_block_height);
+		cal<<<grid,block,size>>>(B,	r,	r,	r,	1,	1, n, d_ptr, pitch, num_block, debug);
+        cudaDeviceSynchronize();
+        if(debug){
+            printf("phase1");
+            test<<<1,1>>>(d_ptr, pitch, n);
+        }
 		/* Phase 2*/
-        cal<<<grid,1024,size>>>(B, r,     r,     0,             r,             1, n, d_ptr, pitch); // left
-		cal<<<grid,1024,size>>>(B, r,     r,  r +1,  round - r -1,             1, n, d_ptr, pitch); // right
-		cal<<<grid,1024,size>>>(B, r,     0,     r,             1,             r, n, d_ptr, pitch); // up
-		cal<<<grid,1024,size>>>(B, r,  r +1,     r,             1,  round - r -1, n, d_ptr, pitch); // down
-        // sync
+        if(r > 0){
+            int num_block_width = ceil(B*r,32);
+            int num_block_height = ceil(B,32);
+            dim3 grid(num_block_width, num_block_height);
+            cal<<<grid,block,size>>>(B, r, r, 0, 1, r, n, d_ptr, pitch, num_block, debug); // up
+        }
+        cudaDeviceSynchronize();
+        if(round-r-1 > 0){
+            int num_block_width = ceil(B*(round-r-1),32);
+            int num_block_height = ceil(B,32);
+            dim3 grid(num_block_width, num_block_height);
+		    cal<<<grid,block,size>>>(B, r, r, r +1, 1, round-r-1, n, d_ptr, pitch, num_block, debug); // down
+        }
+        cudaDeviceSynchronize();
+        if(r > 0){
+            int num_block_width = ceil(B,32);
+            int num_block_height = ceil(B*r,32);
+            dim3 grid(num_block_width, num_block_height);
+		    cal<<<grid,block,size>>>(B, r, 0, r, r, 1, n, d_ptr, pitch, num_block, debug); // left
+        }
+        cudaDeviceSynchronize();
+        if(round-r-1 > 0){
+            int num_block_width = ceil(B,32);
+            int num_block_height = ceil(B*(round-r-1),32);
+            dim3 grid(num_block_width, num_block_height);
+		    cal<<<grid,block,size>>>(B, r, r +1, r, round-r-1, 1, n, d_ptr, pitch, num_block, debug); // right
+        }
+        cudaDeviceSynchronize();
+        if(debug){
+            printf("phase2");
+            test<<<1,1>>>(d_ptr, pitch, n);
+        }
+
 		/* Phase 3*/
-        cal<<<grid,1024,size>>>(B, r,     0,     0,            r,             r, n, d_ptr, pitch); // left-up
-		cal<<<grid,1024,size>>>(B, r,     0,  r +1,  round -r -1,             r, n, d_ptr, pitch); // right-up
-		cal<<<grid,1024,size>>>(B, r,  r +1,     0,            r,  round - r -1, n, d_ptr, pitch); // left-down
-		cal<<<grid,1024,size>>>(B, r,  r +1,  r +1,  round -r -1,  round - r -1, n, d_ptr, pitch); // right-down
+        if(r > 0){
+            int num_block_width = ceil(B*r,32);
+            int num_block_height = ceil(B*r,32);
+            dim3 grid(num_block_width, num_block_height);
+            cal<<<grid,block,size>>>(B, r, 0, 0, r, r, n, d_ptr, pitch, num_block, debug); // left-up
+        }
+        cudaDeviceSynchronize();
+        if(debug){
+            printf("phase3-1");
+            test<<<1,1>>>(d_ptr, pitch, n);
+            cudaDeviceSynchronize();
+        }
+        if((round-r-1 > 0) && (r > 0)){
+            int num_block_width = ceil(B*(round-r-1),32);
+            int num_block_height = ceil(B*r,32);
+            dim3 grid(num_block_width, num_block_height);
+		    cal<<<grid,block,size>>>(B, r, 0, r +1, r, round-r-1, n, d_ptr, pitch, num_block, debug); // left-down
+        }
+        cudaDeviceSynchronize();
+        if(debug){
+            printf("phase3-2");
+            test<<<1,1>>>(d_ptr, pitch, n);
+            cudaDeviceSynchronize();
+        }
+        if((round-r-1 > 0) && (r > 0)){
+            int num_block_width = ceil(B*r,32);
+            int num_block_height = ceil(B*(round-r-1)*r,32);
+            dim3 grid(num_block_width, num_block_height);
+		    cal<<<grid,block,size>>>(B, r, r +1, 0, round-r-1, r, n, d_ptr, pitch, num_block, debug); // right-up
+        }
+        cudaDeviceSynchronize();
+        if(debug){
+            printf("phase3-3");
+            test<<<1,1>>>(d_ptr, pitch, n);
+            cudaDeviceSynchronize();
+        }
+        if(round-r-1 > 0){
+            int num_block_width = ceil(B*(round-r-1),32);
+            int num_block_height = ceil(B*(round-r-1),32);
+            dim3 grid(num_block_width, num_block_height);
+		    cal<<<grid,block,size>>>(B, r, r+1, r+1, round-r-1,  round-r-1, n, d_ptr, pitch, num_block, debug); // right-down
+        }
+        cudaDeviceSynchronize();
+        if(debug){
+            printf("phase3-4");
+            test<<<1,1>>>(d_ptr, pitch, n);
+            cudaDeviceSynchronize();
+        }
         
 	}
+    if(debug)
+        test<<<1,1>>>(d_ptr, pitch, n);
 }
 
 __global__
-void cal(int B, int Round, int block_start_x, int block_start_y, int block_width, int block_height, int n, int *d_ptr, size_t pitch)
+void cal(int B, int Round, int block_start_x, int block_start_y, int block_width, int block_height, int n, int *d_ptr, size_t pitch, int num_block, int debug)
 {
-    printf("QQ");
+    int size = B <= 32 ? B : 32; // block width/height
+    
+    // shared memory
     extern __shared__ int mem[];
     int *dist = mem;
-    int *rowBlock = dist + 1024; 
-    int *colBlock = rowBlock + 32*B*sizeof(int);
-    int num_block = gridDim.x; // num split block, ceil(B/32)
+    int *rowBlock = dist + 1024;
+    int *colBlock = rowBlock + 1024;
 
-    int idx = threadIdx.y*32+threadIdx.x; // internal index
-    int tidx = blockIdx.x*blockDim.x + threadIdx.x; // external index
-    int tidy = blockIdx.y*blockDim.y + threadIdx.y; // external index
+    // num block for each part
+    int num_block_width = gridDim.x;
+    int num_block_height = gridDim.y;
+
+    int idx = threadIdx.y * blockDim.y + threadIdx.x; // internal index, used by dist
+    int tidx = block_start_x * B + 
+               blockIdx.x * blockDim.x + 
+               threadIdx.x; // external index, used by d_ptr
+    int tidy = block_start_y * B +
+               blockIdx.y * blockDim.y + 
+               threadIdx.y; // external index, used by d_ptr
 
     // copy memory from global memory to shared memory
     // copy current block
-    if ((tidx < n) && (tidy < n)){
+    if ((tidx < n) && (tidy < n) && (threadIdx.x < block_width*B) && (threadIdx.y < block_height*B)){
         int *row = (int *)((char*)d_ptr + tidy * pitch);
-        dist[threadIdx.y*32+threadIdx.x] = row[tidx]; 
+        dist[idx] = row[tidx]; 
     }
 
     for (int i = 0; i < num_block; i++){
-
         // copy needed row block
         int idx_x = Round*B + threadIdx.x + i*32;
-        if(idx_x < (Round+1)*B){
+        if((idx_x < (Round+1)*B) && (idx_x < n) && (threadIdx.x < block_width*B) && (threadIdx.y < block_height*B)){
             int *row = (int *)((char*)d_ptr + tidy * pitch);
-            rowBlock[i*1024+threadIdx.y*32+threadIdx.x] = row[idx_x];
+            rowBlock[idx] = row[idx_x];
+            //printf("copy d[%d][%d] = %d\n", tidx, tidy, row[idx_x]);
         }
 
         // copy needed col block
         int idx_y = Round*B + threadIdx.y + i*32;
-        if(idx_y < (Round+1)*B){
+        if((idx_y < (Round+1)*B) && (idx_y < n) && (threadIdx.x < block_width*B) && (threadIdx.y < block_height*B)){
             int *row = (int *)((char*)d_ptr + idx_y * pitch);
-            colBlock[i*1024+threadIdx.y*32+threadIdx.x] = row[threadIdx.x];
+            colBlock[idx] = row[tidx];
+            //printf("copy d[%d][%d] = %d\n", tidx, tidy, row[idx_x]);
         }
+        __syncthreads();
+
+        // current k = Round*B ~ Round*B+size-1
+        for(int k = 0; (k < size) && (Round*B+k < n); k++){
+            if ((tidx < n) && (tidy < n) && (threadIdx.x < block_width*B) && (threadIdx.y < block_height*B)){
+                int ik = rowBlock[threadIdx.y*32+k];
+                int kj = colBlock[k*32+threadIdx.x];
+                if(debug)printf("[%d,%d][epoch=%d, k=%d, ex_tidx=%d, ex_tidy=%d, in_tidx=%d, in_tidy=%d, idx=%d] if %d + %d < %d\n", block_width*B, block_height*B, i, Round*B+i, tidx, tidy, threadIdx.x, threadIdx.y, idx, ik, kj, dist[idx]);
+                if (ik + kj < dist[idx]) {
+                    dist[idx] = ik + kj;
+                }
+            }
+        }
+        __syncthreads();
     }
 
-	int block_end_x = block_start_x + block_height;
-	int block_end_y = block_start_y + block_width;
-	for (int b_i =  block_start_x; b_i < block_end_x; ++b_i) {
-		for (int b_j = block_start_y; b_j < block_end_y; ++b_j) {
-            int block_internal_start_x 	= b_i * B;
-            int block_internal_end_x 	= (b_i +1) * B;
-            int block_internal_start_y  = b_j * B;
-            int block_internal_end_y 	= (b_j +1) * B;
-            if (block_internal_end_x > n)	block_internal_end_x = n;
-            if (block_internal_end_y > n)	block_internal_end_y = n;
-			// To calculate B*B elements in the block (b_i, b_j)
-			// For each block, it need to compute B times
-			for (int k = Round * B; (k < (Round +1) * B) && (k < n); ++k) {
-				// To calculate original index of elements in the block (b_i, b_j)
-				// For instance, original index of (0,0) in block (1,2) is (2,5) for V=6,B=2
-				for (int i = block_internal_start_x; i < block_internal_end_x; ++i) {
-					for (int j = block_internal_start_y; j < block_internal_end_y; ++j) {
-                        int ik = rowBlock[(k-(Round*B))/32*1024+threadIdx.x*32+threadIdx.x];
-                        int kj = colBlock[(k-(Round*B))/32*1024+threadIdx.x*32+threadIdx.x];
-                            printf("if %d + %d < %d\n", ik, kj, dist[idx]);
-						if (ik + kj < dist[idx]) {
-							dist[idx] = ik + kj;
-                        }
-					}
-				}
-			}
-		}
-	}
+    // copy shared memory to global memory
+    if ((tidx < n) && (tidy < n) && (threadIdx.x < block_width*B) && (threadIdx.y < block_height*B)){
+        int *row = (int *)((char*)d_ptr + tidy * pitch);
+        row[tidx] = dist[idx]; 
+    }
 }
 
 
